a14706d0936ddcabe9acfaf34fc2b0e1d29fe06f
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-05-09 06:42:45 +0530

    Add files via upload

---
+++ Interface layer/GUI_class.py
@@ -0,0 +1,11 @@
+# class for the gui:
+import tkinter as tk
+from tkinter import ttk
+import math
+import time
+import threading
+import random
+import cv2
+from PIL import Image, ImageTk
+
+# building the interface class:
